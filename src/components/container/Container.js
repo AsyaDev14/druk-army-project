b69b6ce5be417020df84cbbcc9093ac89b7d9364
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2024-01-31 15:42:04 +0200

    sum

---
+++ src/components/container/Container.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Container.module.css";
+
+const Container = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      {children}
+      Container
+    </div>
+  );
+};
+
+export default Container;
