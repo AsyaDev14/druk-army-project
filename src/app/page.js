74ab8c9936ac10dd2d4548833928d88818e96bd1
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2024-01-31 15:52:08 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-import Section from "./components/Section/Section";
-import Slider from "./components/Slider/Slider";
-import Container from "./components/Container/Container";
+import Section from "../components/Section/Section";
+import Slider from "../components/Slider/Slider";
+import Container from "../components/container/Container";
 
 export default function Home() {
 
