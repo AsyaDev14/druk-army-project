5c3135859e5993133a1bf39ca5e290770f72fd29
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2024-01-31 03:43:46 +0200

    foter

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../app/components/header/Header.jsx";
-import Footer from "../app/components/Footer/Footer.jsx";
+import Footer from "../app/components/footer/Footer.jsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
