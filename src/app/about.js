62bd005c5e50cce3fe37e799a837c3ccfb5b0d63
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2024-01-31 02:34:37 +0200

    initial commit

---
+++ src/app/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RootLayout from './layout.js'
+
+const about = () => {
+  return (
+    <RootLayout>
+      <div>about</div>
+    </RootLayout>
+  )
+}
+
+export default about
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import RootLayout from "./layout";
+
+export default function Home() {
+  return (
+ 
+    <main className={styles.main}>
+      Main
+      </main>
+      
+  );
+}
