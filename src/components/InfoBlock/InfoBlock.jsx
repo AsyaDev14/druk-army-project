05248dc53044507a71fba1376db1120046575cb5
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-02-01 09:17:48 +0200

    add color for second string oftitle

--- src/components/InfoBlock/InfoBlock.jsx
+++ src/components/InfoBlock/InfoBlock.jsx
@@ -1,10 +1,14 @@
 import css from "./InfoBlock.module.css";
 
-const InfoBlock = ({ firstString, secondString, description }) => {
+const InfoBlock = ({ firstString, secondString, description, color }) => {
   return (
     <div className={css.container}>
       {firstString && <h3 className={css.firstString}>{firstString}</h3>}
-      {secondString && <h2 className={css.secondString}>{secondString}</h2>}
+      {secondString && (
+        <h2 className={css.secondString} style={{ color: color }}>
+          {secondString}
+        </h2>
+      )}
 
       {description && <p className={css.description}>{description}</p>}
     </div>
