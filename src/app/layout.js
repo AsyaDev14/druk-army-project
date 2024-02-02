1fb3ad06c7074f8dff0a555068826aea566159d0
Nadya Hristuk <elhe2013@gmail.com>
2024-02-02 03:03:06 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google';
 
-import Header from './components/header/Header.jsx';
-import Footer from './components/footer/Footer.jsx';
+
 
 import './globals.css';
 
@@ -16,9 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Header />
+
         <main>{children}</main>
-        <Footer />
+ 
       </body>
     </html>
   );
