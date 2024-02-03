b3ef81661a2714a766a381c1955c8bdb65f8b141
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-02-03 16:56:13 +0100

    update

--- src/components/header/BurgerButton/BurgerButton.jsx
+++ src/components/header/BurgerButton/BurgerButton.jsx
@@ -1,9 +1,9 @@
 import Icon from '@/components/Icon/Icon'
 import css from './BurgerButton.module.css'
 
-const BurgerButton = () => {
+const BurgerButton = ({onClick}) => {
   return (
-    <button type='button' className={css.burgerBtn}>
+    <button type='button' className={css.burgerBtn} onClick={onClick}>
         <Icon className={css.icon} name={'icon-burger'}/>
     </button>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,23 @@
 "use client";
 
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import BurgerButton from "./BurgerButton/BurgerButton";
 import css from "./Header.module.css";
 import HeaderMenu from "./HeaderMenu/HeaderMenu";
 import HeaderSettings from "./HeaderSettings/HeaderSettings";
 import LoginBtn from "./LoginBtn/LoginBtn";
 import Logo from "./Logo/Logo";
+import MobileMenu from "./MobileMenu/MobileMenu";
 
 const Header = () => {
+  const [isShowMobile, setIsShowMobile] = useState(false);
+  const isMediumTabletEnd = useMediaQuery({maxWidth: 1199.98});
+
+  const handleClickShowMobile = () => {
+    setIsShowMobile(prev => !prev);
+  }
+
   return (
     <header>
       <div className={css.headerContainer}>
@@ -17,8 +27,9 @@ const Header = () => {
           <LoginBtn />
           <HeaderSettings/>
         </div>
-        <BurgerButton />
+        <BurgerButton onClick={handleClickShowMobile}/>
       </div>
+      {isShowMobile && isMediumTabletEnd && <MobileMenu/>}
     </header>
   );
 };
