6ecea160631b9570a509e75d71994f33d3f4b7aa
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-02-09 12:14:05 +0200

    add desktop styles

--- src/app/how-it-works/page.jsx
+++ src/app/how-it-works/page.jsx
@@ -18,47 +18,48 @@ const HowWorks = () => {
       </Container>
       <CentralBlock />
 
-      <div className={css.droneWrapper}>
-      <picture className={css.drone}>
-              <source
-                className={css.drone}
-                media="(max-width: 479.5px)"
-                srcSet="/img/howworks/drone-320.png"
-              />
-              <source
-                className={css.drone}
-                media="(max-width: 767.5px)"
-                srcSet="/img/howworks/drone-480.png"
-              />
-              <source
-                className={css.drone}
-                media="(max-width: 959.5px)"
-                srcSet="/img/howworks/drone-960.png"
-              />
-              <source
-                className={css.drone}
-                media="(max-width: 1199.5px)"
-                srcSet="/img/howworks/drone-960.png"
-              />
-              <source
-                className={css.drone}
-                media="(min-width: 1201px)"
-                srcSet="/img/howworks/drone-1920.png"
-              />
-              <img
-                className={css.drone}
-                src="/img/howworks/drone-1920.png"
-                alt="drone"
-              />
-            </picture>
+      <div className={css.outerWrapper}>
+        <div className={css.droneWrapper}>
+          <picture className={css.drone}>
+            <source
+              className={css.drone}
+              media="(max-width: 479.5px)"
+              srcSet="/img/howworks/drone-320.png"
+            />
+            <source
+              className={css.drone}
+              media="(max-width: 767.5px)"
+              srcSet="/img/howworks/drone-480.png"
+            />
+            <source
+              className={css.drone}
+              media="(max-width: 959.5px)"
+              srcSet="/img/howworks/drone-960.png"
+            />
+            <source
+              className={css.drone}
+              media="(max-width: 1199.5px)"
+              srcSet="/img/howworks/drone-960.png"
+            />
+            <source
+              className={css.drone}
+              media="(min-width: 1201px)"
+              srcSet="/img/howworks/drone-1920.png"
+            />
+            <img
+              className={css.drone}
+              src="/img/howworks/drone-1920.png"
+              alt="drone"
+            />
+          </picture>
 
-
-        <div className={css.accentDiv}>
-          <InfoBlockAccent
-            firstTitle="виріб"
-            secondTitle="для дронів"
-            description="Багато пілотів дронів ризикують, запускаючи дрони з рук. Сьогодні ми можемо це змінити, друкуючи безпечні стартові платформи для дронів. Це не тільки захищає пілотів, але й забезпечує надійний запуск кожного дрона. Ці платформи можуть бути використані в навчальних цілях у школах для майбутніх пілотів"
-          />
+          <div className={css.accentDiv}>
+            <InfoBlockAccent
+              firstTitle="виріб"
+              secondTitle="для дронів"
+              description="Багато пілотів дронів ризикують, запускаючи дрони з рук. Сьогодні ми можемо це змінити, друкуючи безпечні стартові платформи для дронів. Це не тільки захищає пілотів, але й забезпечує надійний запуск кожного дрона. Ці платформи можуть бути використані в навчальних цілях у школах для майбутніх пілотів"
+            />
+          </div>
         </div>
       </div>
 
@@ -70,7 +71,12 @@ const HowWorks = () => {
           />
         </div>
       </Container>
-      <a className={css.button} href="https://app.drukarmy.org.ua">
+      <a
+        className={css.button}
+        href="https://app.drukarmy.org.ua"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Icon className={css.icon} name="icon-people" />
         Долучитися
       </a>
