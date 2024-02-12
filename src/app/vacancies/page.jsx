75812d70f4366fd98f3a2e7a114b50d7ba5d7ab4
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2024-02-12 17:07:01 +0200

    changed static vacancies to dinamic

--- src/app/vacancies/page.jsx
+++ src/app/vacancies/page.jsx
@@ -1,11 +1,4 @@
 import React from "react";
-import officeImg from "../../../public/img/vacancies/office-tab.jpg";
-import office2 from "../../../public/img/vacancies/office2-tab.jpg";
-import ruby from "../../../public/img/vacancies/ruby.jpg";
-import fand from "../../../public/img/vacancies/fand.jpg";
-import manager from "../../../public/img/vacancies/manager.jpg";
-import designer from "../../../public/img/vacancies/designer.jpg";
-import smm from "../../../public/img/vacancies/smm.jpg";
 import Image from "next/image";
 import styles from "@/app/vacancies/Vacancies.module.css";
 import Section from "@/components/Section/Section";
@@ -13,6 +6,72 @@ import Container from "@/components/container/Container";
 import Icon from "@/components/Icon/Icon";
 import Link from "next/link";
 import InfoBlock from "@/components/InfoBlock/InfoBlock";
+
+import officeImg from "../../../public/img/vacancies/office-tab.jpg";
+import office2 from "../../../public/img/vacancies/office2-tab.jpg";
+import ruby from "../../../public/img/vacancies/ruby.jpg";
+import fand from "../../../public/img/vacancies/fand.jpg";
+import manager from "../../../public/img/vacancies/manager.jpg";
+import designer from "../../../public/img/vacancies/designer.jpg";
+import smm from "../../../public/img/vacancies/smm.jpg";
+
+const vacanciesData = [
+  {
+    image: ruby,
+    title: "Ruby-on-Rail-розробник",
+    skills: [
+      "Не менше 4 років професійного досвіду розробки програмного забезпечення;",
+      "Знання Ruby / Ruby on Rails;",
+      "Досвід розробки на основі тестування/поведінки, зокрема RSpec;",
+      "Володіння як документами, так і системами реляційних баз даних (зокрема MySQL, PostgreSQL);",
+      "Перевірена здатність писати читабельний код і ефективне, масштабоване програмне забезпечення.",
+    ],
+    btnText: "Відгукнутися",
+  },
+  {
+    image: fand,
+    title: "Фандрайзер",
+    skills: [
+      "Володіння високим рівнем письмової та усної української та англійської мов;",
+      "Відповідальність, висока мотивація та вміння працювати у стресових ситуаціях та в умовах стислих дедлайнів;",
+      "Вміння добре аналізувати ситуацію, обирати найкращі позиції по проєктах агенції та доносити їх до потенційного спонсора, донора;",
+      "Впевнена експертиза в роботі з грантовими пропозиціями та підготовкою необхідної документації, для самостійного отримання фінансування, так і для залучення підприємства до участі у конкурсі на його отримання.",
+    ],
+    btnText: "Відгукнутися",
+  },
+  {
+    image: smm,
+    title: "SMM-managers",
+    skills: [
+      "Вміння працювати з соціальними мережами та розуміння їх основних принципів;",
+      "Можливість працювати в офісі повний робочий день;",
+      "Високий рівень володіння українською та російською граматикою.",
+    ],
+    btnText: "Відгукнутися",
+  },
+  {
+    image: designer,
+    title: "Графічний дизайнер",
+    skills: [
+      "Навички фотореалістичної візуалізації об'єктів — інтер'єр та екстер'єр, візуалізація -статична. Анімація — буде додатковою перевагою;",
+      "Відмінне знання пакету 3D-візуалізації: SketchUp, 3DSmax+VRay або Corona, Adobe Photoshop, Adobe After effects, бажано Pro на 3ddd;",
+      "Обов’язкове знання меблевих фабрик та виробників різного цінового діапазону, навички комплектації приватних інтер'єрів.",
+    ],
+    btnText: "Відгукнутися",
+  },
+  {
+    image: manager,
+    title: "Менеджер з холодних дзвінків",
+    skills: [
+      "Клієнтоорієнтованість;",
+      "Загальні комунікативні навички, уміння переконувати;",
+      "Вміння презентувати та продавати продукт;",
+      "Вміння продати пісок в пустелі!!!",
+      "Пунктуальна, енергійна і охайна людина.",
+    ],
+    btnText: "Відгукнутися",
+  },
+];
 
 const Vacancies = () => {
   return (
@@ -42,170 +101,54 @@ const Vacancies = () => {
       </Section>
 
       <Section>
-        <div>
-          <InfoBlock
-            firstTitle={"Зараз відкриті"}
-            secondTitle={"Вакансія:"}
-            color={"#5169ff"}
-            description={
-              "Якщо ви вважаєте що можете допомогти, ми раді вітати вас у нашій команді."
-            }
-          />
-          <div className={styles.allVacanciesContainer}>
-            <div className={styles.vacancieContainer}>
-              <Image src={ruby} alt="employee" className={styles.vacancieImg} />
-              <div className={styles.vacancieDescriptionContainer}>
-                <h4 className={styles.vacancieTitle}>Ruby-on-Rail-розробник</h4>
-                <ul>
-                  <p className={styles.skillsTitle}> SKILLS REQUIRED:</p>
-                  <li className={styles.skillsDescription}>
-                    Не менше 4 років професійного досвіду розробки програмного
-                    забезпечення;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Знання Ruby / Ruby on Rails;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Досвід розробки на основі тестування/поведінки, зокрема
-                    RSpec;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Володіння як документами, так і системами реляційних баз
-                    даних (зокрема MySQL, PostgreSQL);
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Перевірена здатність писати читабельний код і ефективне,
-                    масштабоване програмне забезпечення.
-                  </li>
-                </ul>
-                <button className={styles.btnRespond} type="button">
-                  Відгукнутися
-                </button>
-              </div>
-            </div>
-            <div className={styles.vacancieContainer}>
-              <Image src={fand} alt="employee" className={styles.vacancieImg} />
-              <div className={styles.vacancieDescriptionContainer}>
-                <h4 className={styles.vacancieTitle}>Фандрайзер</h4>
-                <ul>
-                  <p className={styles.skillsTitle}> SKILLS REQUIRED:</p>
-                  <li className={styles.skillsDescription}>
-                    Володіння високим рівнем письмової та усної української та
-                    англійської мов;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Відповідальність, висока мотивація та вміння працювати у
-                    стресових ситуаціях та в умовах стислих дедлайнів;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Вміння добре аналізувати ситуацію, обирати найкращі позиції
-                    по проєктах агенції та доносити їх до потенційного спонсора,
-                    донора;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Впевнена експертиза в роботі з грантовими пропозиціями та
-                    підготовкою необхідної документації, для самостійного
-                    отримання фінансування, так і для залучення підприємства до
-                    участі у конкурсі на його отримання.
-                  </li>
-                </ul>
-                <button className={styles.btnRespond} type="button">
-                  Відгукнутися
-                </button>
-              </div>
-            </div>
-            <div className={styles.vacancieContainer}>
-              <Image src={smm} alt="employee" className={styles.vacancieImg} />
-              <div className={styles.vacancieDescriptionContainer}>
-                <h4 className={styles.vacancieTitle}>SMM-managers</h4>
-                <ul>
-                  <p className={styles.skillsTitle}> SKILLS REQUIRED:</p>
-                  <li className={styles.skillsDescription}>
-                    Вміння працювати з соціальними мережами та розуміння їх
-                    основних принципів;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Можливість працювати в офісі повний робочий день;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Високий рівень володіння українською та російською
-                    граматикою.
-                  </li>
-                </ul>
-                <button className={styles.btnRespond} type="button">
-                  Відгукнутися
-                </button>
-              </div>
-            </div>
-            <div className={styles.vacancieContainer}>
-              <Image
-                src={designer}
-                alt="employee"
-                className={styles.vacancieImg}
-              />
-              <div className={styles.vacancieDescriptionContainer}>
-                <h4 className={styles.vacancieTitle}>Графічний дизайнер</h4>
-                <ul>
-                  <p className={styles.skillsTitle}> SKILLS REQUIRED:</p>
-                  <li className={styles.skillsDescription}>
-                    Навички фотореалістичної візуалізації об'єктів — інтер'єр та
-                    екстер'єр, візуалізація -статична. Анімація — буде
-                    додатковою перевагою;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Відмінне знання пакету 3D-візуалізації: SketchUp,
-                    3DSmax+VRay або Corona, Adobe Photoshop, Adobe After
-                    effects, бажано Pro на 3ddd;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Обов’язкове знання меблевих фабрик та виробників різного
-                    цінового діапазону, навички комплектації приватних
-                    інтер'єрів.
-                  </li>
-                </ul>
-                <button className={styles.btnRespond} type="button">
-                  Відгукнутися
-                </button>
-              </div>
-            </div>
-            <div className={styles.vacancieContainer}>
-              <Image
-                src={manager}
-                alt="employee"
-                className={styles.vacancieImg}
-              />
-              <div className={styles.vacancieDescriptionContainer}>
-                <h4 className={styles.vacancieTitle}>
-                  Менеджер з холодних дзвінків
-                </h4>
-                <ul>
-                  <p className={styles.skillsTitle}> SKILLS REQUIRED:</p>
-                  <li className={styles.skillsDescription}>
-                    Клієнтоорієнтованість;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Загальні комунікативні навички, уміння переконувати;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Вміння презентувати та продавати продукт;
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Вміння продати пісок в пустелі!!!
-                  </li>
-                  <li className={styles.skillsDescription}>
-                    Пунктуальна, енергійна і охайна людина.
-                  </li>
-                </ul>
-                <button className={styles.btnRespond} type="button">
-                  Відгукнутися
-                </button>
+        <InfoBlock
+          firstTitle={"Зараз відкриті"}
+          secondTitle={"Вакансія:"}
+          color={"#5169ff"}
+          description={
+            "Якщо ви вважаєте що можете допомогти, ми раді вітати вас у нашій команді."
+          }
+        />
+
+        {vacanciesData.map((vacancy, index) => (
+          <div key={index}>
+            <div className={styles.allVacanciesContainer}>
+              <div className={styles.vacancieContainer}>
+                <Image
+                  src={vacancy.image}
+                  alt="employee img"
+                  className={styles.vacancieImg}
+                />
+                <div className={styles.vacancieDescriptionContainer}>
+                  <h2 className={styles.vacancieTitle}>{vacancy.title}</h2>
+
+                  {vacancy.skills && (
+                    <ul>
+                      <p className={styles.skillsTitle}> SKILLS REQUIRED:</p>
+                      {vacancy.skills.map((skill, skillIndex) => (
+                        <li
+                          key={skillIndex}
+                          className={styles.skillsDescription}
+                        >
+                          {skill}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                  {vacancy.btnText && (
+                    <button className={styles.btnRespond} type="button">
+                      {vacancy.btnText}
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </Section>
+
       <Section>
-        <div>
+        <div className={styles.vacBottomSectionContainer}>
           <div className={styles.imageContainer}>
             <Image
               src={office2}
