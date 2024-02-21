baf63f8d8acd290fcbafc225fef05345a0182857
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-02-21 17:15:18 +0200

    add desctop styles + update Infoblock, infoBlockAccent

--- src/app/[locale]/about/page.jsx
+++ src/app/[locale]/about/page.jsx
@@ -1,26 +1,24 @@
-import Container from "@/components/container/Container.jsx";
 import TitleBlock from "@/components/TitleBlock/TitleBlock.jsx";
 import css from "./page.module.css";
-import ContainerNoPadding from "@/components/ContainerNoPadding/ContainerNoPadding";
 import InfoBlock from "@/components/InfoBlock/InfoBlock";
 import InfoBlockAccent from "@/components/InfoBlockAccent/InfoBlockAccent";
 import Icon from "@/components/Icon/Icon";
 
 const About = () => {
   return (
-    <>
+    <div className={css.pageWrapper}>
       <div className={css.wrapper}>
         <p className={css.backText}>Амбасадори</p>
       </div>
 
       <div className={css.titleTop}>
-        <Container>
+        <div className={css.container}>
           <TitleBlock
             title="Про ДрукАрмію"
             description="ДрукАрмія — це українська спільнота, що протягом року друкує на 3D принтерах необхідне обладнання для ЗСУ, щоб підтримати оборону України проти російських агресорів."
             maxWidth="780px"
           />
-        </Container>
+        </div>
       </div>
 
       <div className={css.about}>
@@ -44,7 +42,7 @@ const About = () => {
             />
             <source
               className={css.photo}
-              media="(max-width: 1200px)"
+              media="(max-width: 1199px)"
               srcSet="/img/about/volnov-960.jpg"
             />
             <source
@@ -91,7 +89,7 @@ const About = () => {
             />
             <source
               className={css.photo}
-              media="(max-width: 1200px)"
+              media="(max-width: 1199px)"
               srcSet="/img/about/sharaievskyi-960.jpg"
             />
             <source
@@ -117,7 +115,7 @@ const About = () => {
         </div>
       </div>
 
-      <ContainerNoPadding>
+      <div className={css.canIWrapper}>
         <div className={css.family}>
           {/* <div className={css.background}></div> */}
           <picture className={css.photo}>
@@ -139,12 +137,12 @@ const About = () => {
             />
             <source
               className={css.photo}
-              media="(max-width: 1200px)"
+              media="(max-width: 1199px)"
               srcSet="/img/about/family-prints-960.jpg"
             />
             <source
               className={css.photo}
-              media="(max-width: 1920px)"
+              media="(max-width: 1919px)"
               srcSet="/img/about/family-prints-1200.jpg"
             />
             <source
@@ -159,61 +157,67 @@ const About = () => {
             />
           </picture>
         </div>
-      </ContainerNoPadding>
 
-      <div className={css.canI}>
-        <Container>
+        <div className={css.canI}>
           <InfoBlock
             firstTitle="Чи можу я"
             secondTitle="друкувати?"
             description="Ми шукаємо свідомих громадян, які мають 3D-принтери та бажання приділити час і докласти зусиль задля допомоги нашим військовим. Ми радо надамо вам консультації та підтримку і допоможемо досягти високої якості друку."
             color="var(--accent-color)"
-            marginLeft="138px"
+            centerAlign={true}
           />
-        </Container>
+        </div>
       </div>
 
-      <div className={css.mapWrapper}>
-        <picture className={css.photo}>
-          <source
-            className={css.photo}
-            media="(max-width: 480px)"
-            srcSet="/img/about/map-320.png"
-          />
+      <div className={css.support}>
+        <div className={css.mapWrapper}>
+          <picture className={css.photo}>
+            <source
+              className={css.photo}
+              media="(max-width: 480px)"
+              srcSet="/img/about/map-320.png"
+            />
 
-          <source
-            className={css.photo}
-            media="(max-width: 767px)"
-            srcSet="/img/about/map-480.png"
-          />
-          <source
-            className={css.photo}
-            media="(max-width: 959px)"
-            srcSet="/img/about/map-768.png"
-          />
-          <source
-            className={css.photo}
-            media="(max-width: 1200px)"
-            srcSet="/img/about/map-960.png"
-          />
-          <source
-            className={css.photo}
-            media="(min-width: 1200px)"
-            srcSet="/img/about/map-1920.png"
-          />
+            <source
+              className={css.photo}
+              media="(max-width: 767px)"
+              srcSet="/img/about/map-480.png"
+            />
+            <source
+              className={css.photo}
+              media="(max-width: 959px)"
+              srcSet="/img/about/map-768.png"
+            />
+            <source
+              className={css.photo}
+              media="(max-width: 1199px)"
+              srcSet="/img/about/map-960.png"
+            />
+            <source
+              className={css.photo}
+              media="(max-width: 1919px)"
+              srcSet="/img/about/map-1200.png"
+            />
+            <source
+              className={css.photo}
+              media="(min-width: 1200px)"
+              srcSet="/img/about/map-1920.png"
+            />
 
-          <img className="" src="/img/about/map-1920.png" alt="map" />
-        </picture>
-      </div>
+            <img className="" src="/img/about/map-1920.png" alt="map" />
+          </picture>
+        </div>
 
-      <div className={css.accent}>
-        <InfoBlockAccent
-          firstTitle="Кого ми"
-          secondTitle="підтримуємо"
-          firstDescription="Обсяг задач, який ми охоплюємо, настільки ж великий, як і наші амбіції. Ми працюємо на потреби захисників на передовій та виконуємо запити штабів ЗСУ. Щодня ми забезпечуємо армію необхідними предметами 3D друку та закриваємо найрізноманітніші потреби. "
-          secondDescription="Ми співпрацюємо з різними організаціями, які самовіддано допомагають українській армії. Ми готові перетворити моток пластику на вкрай необхідне обладнання, що зміцнює оборону нашої країни."
-          marginLeft="223px"
-        />
+        <div className={css.accent}>
+          <div className={css.marginLeft}>
+            <InfoBlockAccent
+              firstTitle="Кого ми"
+              secondTitle="підтримуємо"
+              firstDescription="Обсяг задач, який ми охоплюємо, настільки ж великий, як і наші амбіції. Ми працюємо на потреби захисників на передовій та виконуємо запити штабів ЗСУ. Щодня ми забезпечуємо армію необхідними предметами 3D друку та закриваємо найрізноманітніші потреби. "
+              secondDescription="Ми співпрацюємо з різними організаціями, які самовіддано допомагають українській армії. Ми готові перетворити моток пластику на вкрай необхідне обладнання, що зміцнює оборону нашої країни."
+            />
+          </div>
+        </div>
       </div>
 
       <picture className={css.photo}>
@@ -235,16 +239,25 @@ const About = () => {
         />
         <source
           className={css.photo}
-          media="(max-width: 1200px)"
+          media="(max-width: 1199px)"
           srcSet="/img/about/family-rests-960.jpg"
         />
         <source
           className={css.photo}
-          media="(min-width: 1200px)"
+          media="(max-width: 1919px)"
+          srcSet="/img/about/family-rests-1200.jpg"
+        />
+        <source
+          className={css.photo}
+          media="(min-width: 1920px)"
           srcSet="/img/about/family-rests-1920.jpg"
         />
 
-        <img className="" src="/img/about/family-rests-1920.jpg" alt="family" />
+        <img
+          className={css.photo}
+          src="/img/about/family-rests-1920.jpg"
+          alt="family"
+        />
       </picture>
 
       <div className={css.titleBottom}>
@@ -254,24 +267,26 @@ const About = () => {
         />
       </div>
 
-      <a className={css.button} href="/donations">
-        <Icon className={css.icon} name="icon-banknote" />
-        Підтримати
-      </a>
+      <div className={css.buttonWrapper}>
+        <a className={css.button} href="/donations">
+          <Icon className={css.icon} name="icon-banknote" />
+          Підтримати
+        </a>
 
-      <button
-        className={`${css.button} ${css.secondary}`}
-        // href="/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon
-          className={`${css.icon} ${css.secondary}`}
-          name="icon-mix-lines"
-        />
-        поділитися
-      </button>
-    </>
+        <button
+          className={`${css.button} ${css.secondary}`}
+          // href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon
+            className={`${css.icon} ${css.secondary}`}
+            name="icon-mix-lines"
+          />
+          поділитися
+        </button>
+      </div>
+    </div>
   );
 };
 
