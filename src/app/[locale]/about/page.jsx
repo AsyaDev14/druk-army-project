b05ba9f3299b51e06eef5849447863b8e6fa367f
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-02-26 14:40:39 +0200

    fix styles

--- src/app/[locale]/about/page.jsx
+++ src/app/[locale]/about/page.jsx
@@ -15,47 +15,51 @@ const About = () => {
       <AboutFounders />
       <CanI />
       <Support />
-      <picture className={css.photo}>
-        <source
-          className={css.photo}
-          media="(max-width: 480px)"
-          srcSet="/img/about/family-rests-320.jpg"
-        />
+      <div className={css.container}>
+        <picture className={css.photo}>
+          <source
+            className={css.photo}
+            media="(max-width: 480px)"
+            srcSet="/img/about/family-rests-320.jpg"
+          />
 
-        <source
-          className={css.photo}
-          media="(max-width: 767px)"
-          srcSet="/img/about/family-rests-480.jpg"
-        />
-        <source
-          className={css.photo}
-          media="(max-width: 959px)"
-          srcSet="/img/about/family-rests-768.jpg"
-        />
-        <source
-          className={css.photo}
-          media="(max-width: 1199px)"
-          srcSet="/img/about/family-rests-960.jpg"
-        />
-        <source
-          className={css.photo}
-          media="(max-width: 1919px)"
-          srcSet="/img/about/family-rests-1200.jpg"
-        />
-        <source
-          className={css.photo}
-          media="(min-width: 1920px)"
-          srcSet="/img/about/family-rests-1920.jpg"
-        />
+          <source
+            className={css.photo}
+            media="(max-width: 767px)"
+            srcSet="/img/about/family-rests-480.jpg"
+          />
+          <source
+            className={css.photo}
+            media="(max-width: 959px)"
+            srcSet="/img/about/family-rests-768.jpg"
+          />
+          <source
+            className={css.photo}
+            media="(max-width: 1199px)"
+            srcSet="/img/about/family-rests-960.jpg"
+          />
+          <source
+            className={css.photo}
+            media="(max-width: 1919px)"
+            srcSet="/img/about/family-rests-1200.jpg"
+          />
+          <source
+            className={css.photo}
+            media="(min-width: 1920px)"
+            srcSet="/img/about/family-rests-1920.jpg"
+          />
 
-        <img
-          className={css.photo}
-          src="/img/about/family-rests-1920.jpg"
-          alt="family"
-        />
-      </picture>
-      <TitleBottom />
-      <Buttons />
+          <img
+            className={css.photo}
+            src="/img/about/family-rests-1920.jpg"
+            alt="family"
+          />
+        </picture>
+      </div>
+      <div className={css.reverse}>
+        <TitleBottom />
+        <Buttons />
+      </div>
     </div>
   );
 };
--- src/components/TitleBlock/TitleBlock.jsx
+++ src/components/TitleBlock/TitleBlock.jsx
@@ -2,7 +2,7 @@ import css from "./TitleBlock.module.css";
 
 const TitleBlock = ({ title, description, maxWidth }) => {
   return (
-    <div className={css.container} style={{ maxWidth: maxWidth || "906px" }}>
+    <div className={css.container} style={{ maxWidth: maxWidth}}>
       {title && <p className={css.title}>{title}</p>}
       <div className={css.line}></div>
       {description && <p className={css.description}>{description}</p>}
