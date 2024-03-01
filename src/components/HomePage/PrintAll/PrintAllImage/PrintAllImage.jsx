a39cd99bc23b42567de9df2131e5dc6da6db9300
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-01 08:45:28 +0100

    update

--- src/components/HomePage/PrintAll/PrintAllImage/PrintAllImage.jsx
+++ src/components/HomePage/PrintAll/PrintAllImage/PrintAllImage.jsx
@@ -5,6 +5,7 @@ const PrintAllImage = () => {
   return (
     <div>
         <Image src={"/img/home/sticker.png"} alt='Мультяшний кіт військовий' width={200} height={200}/>
+        <Image src={"/img/home/tweet.png"} alt='Повідомлення в Tweeter під ніком Майор Чорнобаєв' width={200} height={100}/>
     </div>
   )
 }
