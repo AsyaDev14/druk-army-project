61510b167441425f71cdf547646e703103e67f5b
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-01 09:42:35 +0100

    update

--- src/components/HomePage/PrintAll/PrintAllImage/PrintAllImage.jsx
+++ src/components/HomePage/PrintAll/PrintAllImage/PrintAllImage.jsx
@@ -1,13 +1,28 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import css from "./PrintAllImage.module.css"
 
 const PrintAllImage = () => {
   return (
     <div>
-        <Image src={"/img/home/sticker.png"} alt='Мультяшний кіт військовий' width={200} height={200}/>
-        <Image src={"/img/home/tweet.png"} alt='Повідомлення в Tweeter під ніком Майор Чорнобаєв' width={200} height={100}/>
+      <div className={css.imagesContainer}>
+        <Image
+          src={"/img/home/sticker.png"}
+          alt="Мультяшний кіт військовий"
+          width={270}
+          height={319}
+          className={css.catImage}
+        />
+        <Image
+          src={"/img/home/tweet.png"}
+          alt="Повідомлення в Tweeter під ніком Майор Чорнобаєв"
+          width={204.8}
+          height={114.3}
+          className={css.tweet}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PrintAllImage
+export default PrintAllImage;
