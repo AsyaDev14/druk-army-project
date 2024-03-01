970cf2b7639d65c621ea686c7c7ad42545786dc2
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-01 08:42:34 +0100

    update

--- src/components/HomePage/PrintAll/PrintAll.jsx
+++ src/components/HomePage/PrintAll/PrintAll.jsx
@@ -1,6 +1,7 @@
-import css from "./PrintAll.module.css";
 import Container from "@/components/container/Container";
 import PrintAllTitle from "./PrintAllTitle/PrintAllTitle";
+import PrintAllImage from "./PrintAllImage/PrintAllImage";
+import css from "./PrintAll.module.css";
 
 const PrintAll = () => {
   return (
@@ -8,6 +9,7 @@ const PrintAll = () => {
       <Container>
         <PrintAllTitle />
       </Container>
+      <PrintAllImage />
     </div>
   );
 };
---
+++ src/components/HomePage/PrintAll/PrintAllImage/PrintAllImage.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const PrintAllImage = () => {
+  return (
+    <div>
+        <Image src={"/img/home/sticker.png"} alt='Мультяшний кіт військовий' width={200} height={200}/>
+    </div>
+  )
+}
+
+export default PrintAllImage
