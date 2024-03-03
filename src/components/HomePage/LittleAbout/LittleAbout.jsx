71a9deddfcd4d1c2f0061e24a2032b69a0d31a32
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-03 13:43:32 +0100

    update

--- src/components/HomePage/LittleAbout/LittleAbout.jsx
+++ src/components/HomePage/LittleAbout/LittleAbout.jsx
@@ -1,10 +1,12 @@
 import TitleAbout from "./TitleAbout/TitleAbout"
+import Info from "./Info/Info"
 import css from "./LittleAbout.module.css"
 
 const LittleAbout = () => {
   return (
     <div className={css.container}>
         <TitleAbout/>
+        <Info/>
     </div>
   )
 }
