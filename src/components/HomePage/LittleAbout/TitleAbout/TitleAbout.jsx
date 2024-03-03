6d886d2e58da88e7b2a8b2aa74cab2fd08cd11ec
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-03 19:50:49 +0100

    update

--- src/components/HomePage/LittleAbout/TitleAbout/TitleAbout.jsx
+++ src/components/HomePage/LittleAbout/TitleAbout/TitleAbout.jsx
@@ -1,16 +1,18 @@
+"use client"
+
+import { useTranslation } from "react-i18next";
 import css from "./TitleAbout.module.css";
 
 const TitleAbout = () => {
+  const { t } = useTranslation();
   return (
     <div className={css.mainTitleContainer}>
       <h2 className={css.mainTitle}>
-        <span className={css.titleFirst}>трохи цифр</span>
-        <span className={css.titleAccent}>про нас</span>
+        <span className={css.titleFirst}>{t("home:littleNumbers")}</span>
+        <span className={css.titleAccent}>{t("home:aboutUs")}</span>
       </h2>
       <p className={css.mainTitleText}>
-        Спільнота українців, які допомагають армії захищати Україну. Ми нічого
-        не продаємо, всі витрати покриваються коштом друкарів, донорів та
-        волонтерів.
+      {t("home:aboutUsTitleText")}
       </p>
     </div>
   );
