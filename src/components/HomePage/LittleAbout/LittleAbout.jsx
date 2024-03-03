52abc232c5782aa7f09747f27866017613aae2a9
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-03 15:16:25 +0100

    update

--- src/components/HomePage/LittleAbout/LittleAbout.jsx
+++ src/components/HomePage/LittleAbout/LittleAbout.jsx
@@ -1,14 +1,43 @@
-import TitleAbout from "./TitleAbout/TitleAbout"
-import Info from "./Info/Info"
-import css from "./LittleAbout.module.css"
+"use client";
+
+import { useMediaQuery } from "react-responsive";
+import { useTheme } from "next-themes";
+import TitleAbout from "./TitleAbout/TitleAbout";
+import Info from "./Info/Info";
+import Image from "next/image";
+import css from "./LittleAbout.module.css";
 
 const LittleAbout = () => {
+  const { theme } = useTheme();
+  const isTablet = useMediaQuery({ minWidth: 768 });
+  const isTabletEnd = useMediaQuery({ maxWidth: 959.98 });
+
   return (
     <div className={css.container}>
-        <TitleAbout/>
-        <Info/>
+      <TitleAbout />
+      <Info />
+      {isTablet && (
+        <div className={css.girl_container}>
+          <Image
+            src={
+              theme === "light"
+                ? "/img/home/girl-light.png"
+                : "/img/home/girl-dark-theme.png"
+            }
+            alt="гарна анімована дівчина"
+            width={692}
+            height={688}
+            className={css.girl}
+            style={
+              isTablet &&
+              isTabletEnd &&
+              theme === "dark" && { objectPosition: "267px 29px" }
+            }
+          />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default LittleAbout
+export default LittleAbout;
